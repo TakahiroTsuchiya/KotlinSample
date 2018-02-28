678036d9eb54c85c023ca85a59e82031028619df
Takahiro Tsuchiya <takahirotsuchiya@MacBook-Air.local>
2018-02-28 16:37:26 +0900

    ♻️  :recycle: change class name
    - add async request sample code

--- app/src/test/java/com/sample/sampleapplication_madebykotlin/FCMServiceUnitTest.kt
+++ app/src/test/java/com/sample/sampleapplication_madebykotlin/FCMServiceUnitTest.kt
@@ -1,43 +1,57 @@
 package com.sample.sampleapplication_madebykotlin
 
-import com.sample.sampleapplication_madebykotlin.model.Data
-import com.sample.sampleapplication_madebykotlin.model.FCMService
-import com.sample.sampleapplication_madebykotlin.model.Notification
-import com.sample.sampleapplication_madebykotlin.model.NotificationRequest
+import com.sample.sampleapplication_madebykotlin.model.*
+import org.junit.After
+import org.junit.Before
 import org.junit.Test
-import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.Retrofit
-
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.util.concurrent.CountDownLatch
 
 class RCMServiceUnitTest {
 
+    var signal: CountDownLatch? = null
+
+    @Before
+    fun setUp() {
+        println("--- setUp START -----------------")
+        signal = null
+        signal = CountDownLatch(1)
+        println("--- setUp END   -----------------")
+    }
+
+    @After
+    fun tearDown() {
+        println("--- tearDown START -----------------")
+        signal = null
+        println("--- tearDown END   -----------------")
+    }
+
     @Test
-    fun callApi() {
+    fun callApiSync() {
 
-        println("--- callApi Test START -----------------")
-
-        val retrofit = Retrofit.Builder()
-                .baseUrl(FCMService.END_POINT)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-        val service = retrofit.create(FCMService::class.java)
+        println("--- callApiSync Test START -----------------")
 
         val data = Data("title", "body")
         val notification = Notification("title", "body")
-        // TODO
-        val testDevicesIds = arrayOf("")
+        // TODO: Push Send to devices id
+        val to = ""
+        // TODO:
+        val testDevicesIds = arrayOf(to)
         val notificationRequest = NotificationRequest(testDevicesIds, notification, data)
-        // TODO
-        val TEST_API_KEY = "key="
+        // TODO:
+        val TEST_API_KEY = ""
+
+        // API Service を呼び出す
+        val service = FCMApiService.Factory.create()
 
         // sync request
-        val response1 = service.default(FCMService.CONTENT_TYPE_JSON, TEST_API_KEY, notificationRequest).execute()
+        val response = service.sendPushNotification(FCMApiService.contentTypeJson, TEST_API_KEY, notificationRequest).execute()
 
         // HTTP Request
-        if (response1.isSuccessful) {
-            val body = response1.body()
+        if (response.isSuccessful) {
+            val body = response.body()
 
             println("SUCCESS " + body.toString())
 
@@ -53,16 +67,62 @@ class RCMServiceUnitTest {
 
             }
 
-            val errorBody = response1.errorBody()
+            val errorBody = response.errorBody()
             println("ERROR " + errorBody.toString())
 
         } else {
-            val errorBody = response1.errorBody()
+            val errorBody = response.errorBody()
             println("ERROR " + errorBody.toString())
 
         }
 
+        println("--- callApiSync Test END -------------------")
+    }
 
-        print("--- callApi Test END -------------------")
+    @Test
+    fun callApiAsync() {
+
+        println("--- callApiAsync Test START -----------------")
+
+        val data = Data("title", "body")
+        val notification = Notification("title", "body")
+        // TODO: Push Send to devices id
+        val to = ""
+        // TODO:
+        val testDevicesIds = arrayOf(to)
+        val notificationRequest = NotificationRequest(testDevicesIds, notification, data)
+        // TODO:
+        val TEST_API_KEY = ""
+        // API Service を呼び出す
+        val service = FCMApiService.Factory.create()
+
+        val callback = CallbackImpl()
+        callback.signal = signal
+
+        // async request
+        service.sendPushNotification(FCMApiService.contentTypeJson, TEST_API_KEY, notificationRequest).enqueue(callback)
+
+        signal!!.await()
+        println("--- callApiAsync Test END -------------------")
+    }
+
+
+    private class CallbackImpl() : Callback<NotificationResponse> {
+
+        var signal: CountDownLatch? = null
+
+        init {
+            println("--- init ------------")
+        }
+
+        override fun onFailure(call: Call<NotificationResponse>?, t: Throwable?) {
+            println("--- onFailure ------------")
+            signal!!.countDown()
+        }
+
+        override fun onResponse(call: Call<NotificationResponse>?, response: Response<NotificationResponse>?) {
+            println("--- onResponse ------------")
+            signal!!.countDown()
+        }
     }
 }
